87994bce02eb449503e3c73b4aff01d3641bcfc2
Muzzamil hassan <129590453+muzzamilhassan@users.noreply.github.com>
2024-03-17 13:38:27 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ReCAPTCHA from "react-google-recaptcha";
+function App() {
+
+
+  return (
+    <>
+     <ReCAPTCHA
+    sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+  />
+    </>
+  )
+}
+
+export default App
